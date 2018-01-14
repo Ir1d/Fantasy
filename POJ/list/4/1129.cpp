f42a84b77a6f9ccad499e67d2c8b04bdf1bebfa2
Ir1d <sirius.caffrey@gmail.com>
2018-01-14 18:05:55 +0800

    Update

--- POJ/list/4/1129.cpp
+++ POJ/list/4/1129.cpp
@@ -5,7 +5,6 @@
  */
 #include <cstdio>// NOLINT
 #include <cstring>// NOLINT
-#include <bits/stdc++.h>// NOLINT
 #include <cmath>// NOLINT
 #define x1 x11
 #define y1 y11
@@ -32,14 +31,56 @@ const double pi = acos(-1.0);
 const double EPS = 1e-8;
 const int MAXN = 100033;
 
-
+int n;
+char s[33];
+int map[33][33];
+int u, v;
+int c[33];
+bool ok(int x) {
+  f(i, 0, n) if (i != x && map[i][x]) {
+    if (c[x] == c[i]) return 0;
+  }
+  return 1;
+}
+bool dfs(int x, int cNum) {
+  if (x >= n) return 1;
+  g(i, 1, cNum) {
+    c[x] = i;
+    if (ok(x)) {
+      if (dfs(x + 1, cNum)) return 1;
+    }
+    c[x] = 0;
+  }
+  return 0;
+}
 int main() {
 #ifdef LOCAL
   freopen("a.in", "r", stdin);
   freopen("a.out", "w", stdout);
 #endif
 
-
+  while (~scanf("%d", &n) && n) {
+    memset(map, 0, sizeof map);
+    f(i, 0, n) {
+      scanf("%s", s);
+      u = s[0] - 'A';
+      f(j, 2, strlen(s)) {
+        v = s[j] - 'A';
+        map[v][u] = map[u][v] = 1;
+      }
+    }
+    memset(c, 0, sizeof c);
+    g(i, 1, n) {
+      if (dfs(0, i)) {
+        if (i == 1) {
+          puts("1 channel needed.");
+        } else {
+          printf("%d channels needed.\n", i);
+        }
+        break;
+      }
+    }
+  }
 
 #ifdef LOCAL
   fclose(stdin);
